c11db5a984022bce53e2e7b8f388f3d58d62d4ea
avaness <austinvaness@gmail.com>
2024-08-23 22:42:35 -0500

    Update checkout pages

--- js/license.js
+++ js/license.js
@@ -1,5 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const licenseKey = urlParams.get('key');
+const licenseTier = urlParams.get('tier');
 const guidRegex = /^[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}$/;
 if(!licenseKey)
 {
@@ -8,7 +9,9 @@ if(!licenseKey)
 else if(licenseKey !== "00000000-0000-0000-0000-000000000000" && guidRegex.test(licenseKey))
 {
 	addEventListener("load", () => {
-		document.querySelector("#license-key").innerHTML = licenseKey.toUpperCase();
+		document.querySelector("#license-key").innerText = licenseKey.toUpperCase();
+		if(licenseTier && licenseTier.length > 1)
+			document.querySelector("#license-tier").innerText = licenseTier.charAt(0).toUpperCase() + licenseTier.slice(1).toLowerCase();
 	});
 }
 else
--- js/upgrade.js
+++ js/upgrade.js
@@ -1,12 +1,12 @@
 const urlParams = new URLSearchParams(window.location.search);
 const licenseTier = urlParams.get('tier');
-if(!licenseTier)
+if(!licenseTier || licenseTier.length < 2)
 {
 	window.location.replace("/404");
 }
 else
 {
 	addEventListener("load", () => {
-		document.querySelector("#license-tier").innerHTML = licenseTier.toUpperCase();
+		document.querySelector("#license-tier").innerText = licenseTier.charAt(0).toUpperCase() + licenseTier.slice(1).toLowerCase();
 	});
 }
