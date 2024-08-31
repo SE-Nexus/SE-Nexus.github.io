22a15001cbed0596014a81bea707890d49b11cb8
avaness <austinvaness@gmail.com>
2024-08-30 22:56:07 -0500

    More countdown tweaks

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-
-// Set the date we're counting down to
 const countDownDate = new Date("2024-09-07T01:00Z").getTime();
 var interval;
 var countdownElement;
@@ -19,10 +17,6 @@ function onInterval()
 	var now = new Date().getTime();
 	var distance = countDownDate - now;
 
-	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 	if (distance <= 0) 
 	{
@@ -31,6 +25,42 @@ function onInterval()
 	}
 	else 
 	{
-		countdownElement.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		var timer = "";
+		if (days > 0)
+		{
+			timer = "" + days;
+			if(days > 1)
+				timer += " days";
+			else
+				timer += " day";
+		}
+		else
+		{
+			timer = "";
+
+			if(hours > 0)
+			{
+				timer += hours + ":";
+				if(minutes < 10)
+					timer += "0";
+				timer += minutes + ":";
+				if(seconds < 10)
+					timer += "0";
+			}
+			else if (minutes > 0)
+			{
+				timer += minutes + ":"
+				if(seconds < 10)
+					timer += "0";
+			}
+
+			timer += seconds;
+		}
+		countdownElement.innerHTML = timer;
 	}
 }
